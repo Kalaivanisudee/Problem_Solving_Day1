d3db4d783b5be74850d73ea0406f20713e2ce601
kalaivanisudee <gkalaisakthi22@gmail.com>
2024-05-16 12:42:16 +0530

    fix:code optimized

--- sorting_an_array.js
+++ sorting_an_array.js
@@ -4,16 +4,14 @@ let arr = [5,1,8,4,3];
 console.log(arr.length);// Output:5
 
 for(let i =0 ; i<arr.length; i++){
-    let minimum = arr[i];
     let minIndex = i;
     for ( let j =i+1 ; j<arr.length; j++){
-        if(minimum>arr[j]){ 
-            minimum = arr[j];
+        if(arr[minIndex]>arr[j]){ 
             minIndex = j;
         }
     }
     temp=arr[i];
-    arr[i]=minimum;
+    arr[i]=arr[minIndex];
     arr[minIndex]=temp;
 }
 
