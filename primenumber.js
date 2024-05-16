c10c39a574788961f2d1e8fb176fb079d5814702
kalaivanisudee <gkalaisakthi22@gmail.com>
2024-05-16 18:45:23 +0530

    fix:found prime number if input is oddnumber

--- primenumber.js
+++ primenumber.js
@@ -1,7 +1,10 @@
 //what is prime?
 // A number is only divisible by 1 and itself
 //2,3,5,7,11,13,17,19,23....
-//Any EvenNumber given as input it will give output as false for finding prime number except 2, because it's evenprimnumber
+/*
+*Any EvenNumber given as input it will give output as false for finding prime number except 2, because it's evenprimnumber
+*/
+//
 let count = 0;
 function isPrime(num) {
   for (let fact = 2; fact < num; fact++) {
@@ -13,8 +16,6 @@ function isPrime(num) {
   return true;
 }
 
-console.log("Given EvenNumber is prime number?: ", isPrime(100)); //false
-console.log("Number of Iteration: ", count); //Number of Iteration:  1
+console.log("Given OddNumber is prime number?: ", isPrime(103)); //true
+console.log("Number of Iteration: ", count); //Number of Iteration:  101
 
-console.log("Given EvenNumber is prime number?: ", isPrime(2)); //true
-console.log("Number of Iteration: ", count); //Number of Iteration:  1
